6f1f7d08646d241b76fb017b7f1bfc2baaf81d29
devgis <devgis@qq.com>
2017-10-16 17:02:44 +0800

    DataGridView-Waterfall
    DataGridView瀑布流

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(929, 266);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.dataGridView1_Scroll);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 266);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Form1_Scroll);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            this.Text = "[New]:" + e.NewValue.ToString() + " [OldValue]:" + e.OldValue.ToString() + " [ScrollOrientation]:" + e.ScrollOrientation.ToString() + " [Type]:" + e.Type.ToString();
+        }
+
+        private void Form1_Scroll(object sender, ScrollEventArgs e)
+        {
+            this.Text = "[New]:" + e.NewValue.ToString() + " [OldValue]:" + e.OldValue.ToString() + " [ScrollOrientation]:" + e.ScrollOrientation.ToString() + " [Type]:" + e.Type.ToString();
+        }
+        int oldCount = 0;
+        bool bLoad = false;
+        private void dataGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+            if (dataGridView1.FirstDisplayedScrollingRowIndex >= dataGridView1.Rows.Count - 50)
+            {
+                //dataGridView1.Enabled = false;
+                MessageBox.Show("加载中...........");
+                for (int i = 0; i < 100; i++)
+                {
+                    dataGridView1.Rows.Add("瀑布流出来的" + i, "bbb" + i);
+                }
+                //dataGridView1.Enabled = true;
+            }
+            bLoad = true;
+            
+            ////if (dataGridView1.FirstDisplayedScrollingRowIndex != oldCount)
+            ////{
+            //if (!bLoad)
+            //{
+            //    dataGridView1.Enabled = false;
+            //    if (dataGridView1.FirstDisplayedScrollingRowIndex >= dataGridView1.Rows.Count - 50)
+            //    {
+            //        for (int i = 0; i < 100; i++)
+            //        {
+            //            dataGridView1.Rows.Add("瀑布流出来的" + i, "bbb" + i);
+            //        }
+            //    }
+            //    bLoad = true;
+            //    dataGridView1.Enabled = true;
+            //}
+            //else
+            //{
+            //    bLoad = false;
+            //}
+            ////}
+            ////this.Text = "[New]:" + e.NewValue.ToString() + " [OldValue]:" + e.OldValue.ToString() + " [ScrollOrientation]:" + e.ScrollOrientation.ToString() + " [Type]:" + e.Type.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100;i++ )
+            {
+                dataGridView1.Rows.Add("aaaa" + i, "bbb" + i);
+            }
+            
+        }
+
+        private void radGridView1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
